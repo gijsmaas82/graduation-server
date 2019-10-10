6bb183bea66aab0d96912f65ece54e54d044cca1
gijsmaas82 <gijsmaas82@outlook.com>
2019-10-10 16:58:08 +0200

    showtime

--- src/Drawings/router.js
+++ src/Drawings/router.js
@@ -19,4 +19,12 @@ async function newDrawing (req, res) {
 
 router.post('/drawing', auth, newDrawing)
 
+async function getDrawings (req, res) {
+  const drawings = await Drawing.findAll({ where: { userId: req.user.id}})
+
+  return res.send(drawings)
+}
+
+router.get('/drawings', auth, getDrawings)
+
 module.exports = router
